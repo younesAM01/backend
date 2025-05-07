88f7f5d606daaa8ea9a15c52fa2c5f9e5f16d39b
younesAM01 <younesaitmanssour@gmail.com>
2025-05-08 00:44:51 +0100

    new data upda

--- database/mongodb.js
+++ database/mongodb.js
@@ -5,44 +5,14 @@ if (!DB_URI) {
   throw new Error("please set the DB_URI environment variable");
 }
 
-let isConnected = false;
-
 const connectDB = async () => {
-  if (isConnected) {
-    console.log("Using existing database connection");
-    return true;
-  }
-
   try {
-    const db = await mongoose.connect(DB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      serverSelectionTimeoutMS: 5000,
-      socketTimeoutMS: 45000,
-      family: 4,
-      maxPoolSize: 10,
-      minPoolSize: 5,
-      connectTimeoutMS: 10000,
-    });
-    
-    isConnected = db.connections[0].readyState === 1;
+    await mongoose.connect(DB_URI);
     console.log("Connected to MongoDB");
-    return isConnected;
   } catch (error) {
     console.error("Error connecting to MongoDB:", error);
-    return false;
   }
 };
-
-mongoose.connection.on('error', (err) => {
-  console.error('MongoDB connection error:', err);
-  isConnected = false;
-});
-
-mongoose.connection.on('disconnected', () => {
-  console.log('MongoDB disconnected');
-  isConnected = false;
-});
 
 export default connectDB;
 
