e4350692ef8abf41ea0526c66166f6f27364dc42
younesAM01 <younesaitmanssour@gmail.com>
2025-05-28 21:39:26 +0100

    payment add

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://stayfit.sa/en",
+    origin: "https://stayfit.sa",
     credentials: true,
   })
 );
